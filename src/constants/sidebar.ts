401faa5a51d874a47f1c1133ea36208713154271
jasperchess <chesselet.jasper@gmail.com>
2025-06-17 17:19:41 +0200

    initial commit

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,62 @@
+const SIDEBAR = [
+  {
+    title: "Introduction",
+    items: [
+      {
+        title: "Getting Started",
+        href: "/documentation/gettingstarted",
+      },
+      {
+        title: "Installation",
+        href: "/documentation/installation",
+      },
+      {
+        title: "Quickstart",
+        href: "/documentation/quickstart",
+      },
+    ],
+  },
+
+  {
+    title: "Guides",
+    items: [
+      {
+        title: "SDKs",
+        href: "/documentation/sdks",
+      },
+      {
+        title: "Authentication",
+        href: "/documentation/authentication",
+      },
+      {
+        title: "Error Handling",
+        href: "/documentation/errorhandling",
+      },
+      {
+        title: "Hooks",
+        href: "/documentation/hooks",
+        label: true,
+      },
+    ],
+  },
+
+  {
+    title: "Contribute",
+    items: [
+      {
+        title: "How to contribute",
+        href: "/documentation/how-to-contribute",
+      },
+      {
+        title: "Contributing guideline",
+        href: "/documentation/contributing-guideline",
+      },
+      {
+        title: "Having an issue",
+        href: "/documentation/having-an-issue",
+      },
+    ],
+  },
+];
+
+export default SIDEBAR;
